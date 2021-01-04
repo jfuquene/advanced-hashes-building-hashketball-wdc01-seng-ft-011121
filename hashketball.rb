c55901ab55429d68689100a05eff924d194d21ed
John Fuquene <jfuquene@email.wm.edu>
2021-01-04 20:22:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,19 +1,102 @@
 def game_hash 
   game_hash = { 
     :home => {
-      :team_name => 
+      :team_name => "Brooklyn Nets"
       :colors => ["Blacks", "White"]
-      :players => [{
-        :players_name => "" 
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds =>
-        :assist => 
-        :steals => 
-        :blocks => 
-        :slam_dunks => 
-      }]
+      :players => [
+        {
+        :players_name => "Alan Anderson",
+        :number => 0, 
+        :shoe => 16, 
+        :points => 22,
+        :rebounds => 12, 
+        :assist => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      }, 
+      
+      {
+        :players_name => "Reggie Evans",
+        :number => 30, 
+        :shoe => 14, 
+        :points => 12,
+        :rebounds => 12, 
+        :assist => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      }, 
+      { 
+        :players_name => "Brook Lopez",
+        :number => 11, 
+        :shoe => 17, 
+        :points => 17,
+        :rebounds => 19, 
+        :assist => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
+      }, 
+      { 
+        :players_name => "Mason Plumlee",
+        :number => 1, 
+        :shoe => 19, 
+        :points => 26,
+        :rebounds => 11, 
+        :assist => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5
+      }
+      {
+        :players_name => "Jason Terry",
+        :number => 31, 
+        :shoe => 15, 
+        :points => 19,
+        :rebounds => 2, 
+        :assist => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
+      ]
     }
     :away => {
-    }
+      :team_name => "Charlotte Hornets"
+      :colors => ["Turquoise", "Purple "]
+      :players => [
+        {
+        :players_name => "Jeff Adrien",
+        :number => 4, 
+        :shoe => 18, 
+        :points => 10,
+        :rebounds => 1, 
+        :assist => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+      }, 
+      
+      {
+        :players_name => "Bismack Biyombo",
+        :number => 0, 
+        :shoe => 16, 
+        :points => 12,
+        :rebounds => 4, 
+        :assist => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+        
+      }, 
+      { 
+        :players_name => "DeSagna Diop",
+        :number => 2, 
+        :shoe => 14, 
+        :points => 24,
+        :rebounds => 12, 
+        :assist => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
+        }
