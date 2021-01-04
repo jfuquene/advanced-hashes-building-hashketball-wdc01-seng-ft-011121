0677f686157f4deb194c669d3adb822185cef4f4
John Fuquene <jfuquene@email.wm.edu>
2021-01-04 20:08:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,2 +1,19 @@
 def game_hash 
   game_hash = { 
+    :home => {
+      :team_name => 
+      :colors => ["Blacks", "White"]
+      :players => [{
+        :players_name => 
+        :number => 
+        :shoe => 
+        :points => 
+        :rebounds =>
+        :assist => 
+        :steals => 
+        :blocks => 
+        :slam_dunks => 
+      }]
+    }
+    :away => {
+    }
